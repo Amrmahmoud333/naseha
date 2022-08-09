1f685cc42c6248f28a5ec2f8c062e2ad440b755a
amrmahmoud333 <3mrma7moud69@gmail.com>
2022-08-09 18:27:07 +0200

    add validation

--- lib/views/auth/login/widgets/login_form.dart
+++ lib/views/auth/login/widgets/login_form.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:naseha/views/shared/rounded_clipper.dart';
 
 class LoginFormWidget extends StatelessWidget {
-  const LoginFormWidget({
+  LoginFormWidget({
     Key? key,
     required this.emailController,
     required this.passwordController,
@@ -10,7 +10,10 @@ class LoginFormWidget extends StatelessWidget {
 
   final TextEditingController emailController;
   final TextEditingController passwordController;
-
+  final formKey = GlobalKey<FormState>();
+  bool emailValidator({required String email}) => RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+      .hasMatch(email);
   @override
   Widget build(BuildContext context) {
     double h(double n) {
@@ -29,61 +32,73 @@ class LoginFormWidget extends StatelessWidget {
         decoration: const BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(40)),
             color: Colors.white),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: h(90)),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: w(20)),
-              child: TextField(
-                controller: emailController,
-                style: const TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  hintText: 'البريد الالكتروني',
-                  helperStyle: TextStyle(
-                    color: Colors.blue.shade200,
-                  ),
-                  border: InputBorder.none,
-                  icon: const Icon(
-                    Icons.email,
-                    color: Color(0xff336B87),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding:
-                  EdgeInsets.only(left: w(20), right: w(20), bottom: h(10)),
-              child: const Divider(
-                color: Color(0xff336B87),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: w(20)),
-              child: TextField(
-                controller: passwordController,
-                style: const TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  hintText: 'كلمة المرور',
-                  helperStyle: TextStyle(
-                    color: Colors.blue.shade200,
-                  ),
-                  border: InputBorder.none,
-                  icon: const Icon(
-                    Icons.lock,
-                    color: Color(0xff336B87),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: h(90)),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: w(20)),
+                child: TextFormField(
+                  controller: emailController,
+                  validator: (email) => emailValidator(email: email!)
+                      ? 'Enter a valid email'
+                      : null,
+                  style: const TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: 'البريد الالكتروني',
+                    helperStyle: TextStyle(
+                      color: Colors.blue.shade200,
+                    ),
+                    border: InputBorder.none,
+                    icon: const Icon(
+                      Icons.email,
+                      color: Color(0xff336B87),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              padding:
-                  EdgeInsets.only(left: w(20), right: w(20), bottom: h(10)),
-              child: const Divider(
-                color: Color(0xff336B87),
+              Container(
+                padding:
+                    EdgeInsets.only(left: w(20), right: w(20), bottom: h(10)),
+                child: const Divider(
+                  color: Color(0xff336B87),
+                ),
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: w(20)),
+                child: TextFormField(
+                  controller: passwordController,
+                  textInputAction: TextInputAction.next,
+                  obscureText: true,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: ((value) => value != null && value.length > 7
+                      ? 'Enter more than 7 characters'
+                      : null),
+                  style: const TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: 'كلمة المرور',
+                    helperStyle: TextStyle(
+                      color: Colors.blue.shade200,
+                    ),
+                    border: InputBorder.none,
+                    icon: const Icon(
+                      Icons.lock,
+                      color: Color(0xff336B87),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding:
+                    EdgeInsets.only(left: w(20), right: w(20), bottom: h(10)),
+                child: const Divider(
+                  color: Color(0xff336B87),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

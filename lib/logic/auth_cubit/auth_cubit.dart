d4c6e61fcd15000300154fc6bfe1e9f99b00876e
amrmahmoud333 <3mrma7moud69@gmail.com>
2022-08-10 17:35:52 +0200

    add init design to verify screen

--- lib/logic/auth_cubit/auth_cubit.dart
+++ lib/logic/auth_cubit/auth_cubit.dart
@@ -2,7 +2,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
 import 'package:naseha/data/repositories/firebase_auth_repo.dart';
 

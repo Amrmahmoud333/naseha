4d39356db87e239700454880a2fbd20b85bd214a
amrmahmoud333 <3mrma7moud69@gmail.com>
2022-11-14 18:47:38 +0200

    edit email verifiy

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ import 'package:naseha/logic/auth_cubit/auth_cubit.dart';
 import 'package:naseha/views/auth/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:naseha/views/home_page/home_page.dart';
+import 'package:naseha/views/verify/verify_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   runApp(const MyApp());
 }
 
@@ -32,8 +32,12 @@ class MyApp extends StatelessWidget {
         home: StreamBuilder<User?>(
             stream: FirebaseAuth.instance.authStateChanges(),
             builder: (context, snapshot) {
-              if (snapshot.hasData) {
+              if (snapshot.hasData &&
+                  FirebaseAuth.instance.currentUser!.emailVerified) {
                 return const HomePage();
+              } else if (snapshot.hasData &&
+                  !(FirebaseAuth.instance.currentUser!.emailVerified)) {
+                return const VerifyScreen();
               } else {
                 return const AuthPage();
               }
